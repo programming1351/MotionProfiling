f2e72856de7f27ea088a972b78b708412dcbbde6
DhruvBharadwaj25 <dhruvbharadwaj25@mittymonarch.com>
2023-01-24 16:56:22 -0800

    starting push

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -22,31 +23,33 @@ public class Robot extends TimedRobot {
   TrapezoidProfile.Constraints constraints;
   TrapezoidProfile profile;
   TrapezoidProfile.State profileOutput;
-  WPI_TalonFX falcon;
-  WPI_TalonFX falcon2;
-  WPI_TalonFX falcon3;
-  WPI_TalonFX falcon4;
-  double output;
-
-  PIDController controller;
+  Spark sparkRight;
+  Spark sparkLeft;
   Encoder encoder;
-  double t;
-
-  hiii 
+  double kp, ki, kd;
 
 
   @Override
   public void robotInit() {
-    falcon = new WPI_TalonFX(0);
-    falcon2 = new WPI_TalonFX(1);
-    falcon3 = new WPI_TalonFX(1);
-    falcon4 = new WPI_TalonFX(1);
-    start = new TrapezoidProfile.State(0, 0);
-    end = new TrapezoidProfile.State(5.0, 0);
-    constraints = new TrapezoidProfile.Constraints(2, 0.2);
+    sparkRight = new Spark(0);
+    sparkLeft = new Spark(1);
+    //initializing sparks
+
+    encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
+    //encoder initialization
+
+    start = new TrapezoidProfile.State(0,0);
+    end = new TrapezoidProfile.State(1.0,0);
+    constraints = new TrapezoidProfile.Constraints(0.2, 0.2);
     profile = new TrapezoidProfile(constraints, end, start);
-    double kp = 0.1, ki = 0, kd = 0;
-    controller = new PIDController(kp, ki, kd);
+    profileOutput = profile.calculate(0.2);
+    //initialization of TrapezoidProfile states
+
+    kp = 0;
+    ki = 0;
+    kd = 0;
+    //ki kp kd vals
+    
   }
 
 
@@ -75,18 +78,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
-    TrapezoidProfile.State setpoint = profile.calculate(t);
-    controller.setSetpoint(setpoint.velocity);
-    output = controller.calculate(falcon.getSelectedSensorPosition() / 166);
-    falcon.set(ControlMode.PercentOutput, output);
-    output = controller.calculate(falcon2.getSelectedSensorPosition() / 166);
-    falcon2.set(ControlMode.PercentOutput, output);
-    output = controller.calculate(falcon3.getSelectedSensorPosition() / 166);
-    falcon3.set(ControlMode.PercentOutput, output);
-    output = controller.calculate(falcon4.getSelectedSensorPosition() / 166);
-    falcon4.set(ControlMode.PercentOutput, output);
-    t += 0.02;
 
   }
 
